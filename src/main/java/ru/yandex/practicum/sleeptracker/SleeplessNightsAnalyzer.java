6a4ff3fec82979af93d58ee1e27aa953b455f5e3
4315 Ледовских А.В. <ledartem@bk.ru>
2025-11-25 22:09:01 +0300

    Fifth version of Sprint 8

--- src/main/java/ru/yandex/practicum/sleeptracker/SleeplessNightsAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleeplessNightsAnalyzer.java
@@ -6,7 +6,6 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 public class SleeplessNightsAnalyzer implements SleepAnalyzer {
 
